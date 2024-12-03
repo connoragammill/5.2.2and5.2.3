51cfa40dd32b46308a2445dcd5fc1f2651b06737
connor gammill <connor.gammill@outlook.com>
2024-12-03 12:23:43 -0500

    Add project files.

---
+++ 5.2.2and.3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _5._2._2and._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = Convert.ToInt32(Console.ReadLine());//input the first number
+            RecursiveNumbers(numbers);//Call the method
+            int numbers1 = Convert.ToInt32(Console.ReadLine());//input the second number
+            NewRecursiveNumbers(numbers1); //Call the method 
+        }
+        static void RecursiveNumbers(int natNums)
+        {
+            if (natNums > 0)//if the natural number is greater than 0
+            {
+                RecursiveNumbers(natNums - 1);//Recursion method 
+                Console.WriteLine(natNums);//Putting the console writeLine below the recursion method makes it count up from 1 till it gets to the number
+			}
+        }
+
+        static void NewRecursiveNumbers (int natNums1)//if the natural number is greater than 0
+		{
+            if ( natNums1 > 0)//if the natural number is greater than 0
+			{
+                Console.WriteLine(natNums1);//Putting the console writeLine above the recursion method makes it count down till it gets to 1
+                NewRecursiveNumbers(natNums1 - 1);//Recursion method
+            }
+        }
+
+    }
+}
